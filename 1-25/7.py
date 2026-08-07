e84f47c74e176fba069a6499e9db63403f3d2fff
Samir <amirkingnaro@gmail.>
2026-08-07 06:05:38 +0530

    Day 7 - Functional Flexibility with args and  kwargs

---
+++ 1-25/7.py
@@ -0,0 +1,21 @@
+
+def calculate_power_sum(*numbers:int) -> int:
+    """Calculate the sum of cube of the sum of the given numbers. """
+    total = sum(numbers)
+    return total **3
+
+def generate_hero_stats(hero_name: str, **attributes: int) -> dict:
+    """Generate a dictionary of hero attributes."""
+    stats = {"Hero Name": hero_name}
+    stats.update(attributes)
+    return stats
+
+def main() -> None:
+    power = calculate_power_sum(1, 2, 3)
+    print(f"Power Sum: {power}")
+
+    hero_stats = generate_hero_stats("Superman", strength=100, speed=90, intelligence=85)
+    print(f"Hero Stats: {hero_stats}")
+
+if __name__ == "__main__":
+    main()
