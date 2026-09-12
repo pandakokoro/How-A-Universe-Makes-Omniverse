eb77f46b5b344910c85d3088cab1ab8460cf545a
Samir <amirkingnaro@gmail.>
2026-09-12 20:53:29 +0530

    feat : modified the code with def function as first code of the 1000's code.

--- 1-25/1.py
+++ 1-25/1.py
@@ -1,2 +1,20 @@
-pycom = input('what you can give this universe by \n your ultimate message: ')
-print(pycom)
+import random as message
+
+msg = message.choice(["dare","truth"])
+
+def sovereign_msg(msg):
+    pycom = input(f'what you can give this universe, \n write your ultimate message: {msg =} : ')
+    print(pycom)
+    soverignty = ["save","help","we","india","heart","peace"]
+    if "indian" in pycom:
+        if any(word in pycom for word in soverignty):
+            print("soverignty has been called")
+        else:
+            pass
+    else:
+        return
+
+if __name__ == "__main__" :
+    sovereign_msg(msg)
+
+        
